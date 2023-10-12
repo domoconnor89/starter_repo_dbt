1985d88fb240785f20844839903680979eb6f12d
domoconnor89 <dominicoconnor89@googlemail.com>
2023-10-12 17:33:57 +0200

    staging updated

--- models/staging/staging_weather.sql
+++ models/staging/staging_weather.sql
@@ -1,3 +1,4 @@
+/*
 WITH temperature_daily AS (
     SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date  AS date,
         ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
@@ -11,3 +12,31 @@ WITH temperature_daily AS (
     FROM {{source("staging", "raw_temp")}})
 SELECT * 
 FROM temperature_daily
+*/
+
+WITH temperature_daily AS (
+    SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date  AS date,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
+        (extracted_data -> 'location' -> 'name')::VARCHAR  AS city,
+        (extracted_data -> 'location' -> 'region')::VARCHAR  AS region,
+        (extracted_data -> 'location' -> 'country')::VARCHAR  AS country,
+        ((extracted_data -> 'location' -> 'lat')::VARCHAR)::NUMERIC  AS lat, 
+        ((extracted_data -> 'location' -> 'lon')::VARCHAR)::NUMERIC  AS lon
+    FROM {{source("staging", "raw_temp")}}),
+        temperatutre_daily_updated AS (
+            SELECT 
+                date,
+                maxtemp_c as maxtemp_c,
+                mintemp_c,
+                avgtemp_c,,
+                substring(city, 1, (length(city)-2)) as city,
+                substring(region, 1, (length(region)-2)) as region,
+                substring(country, 1, (length(country)-2)) as country,
+                lat,
+                lon
+            FROM temperature_daily
+)
+SELECT *
+FROM temperatutre_daily_updated
