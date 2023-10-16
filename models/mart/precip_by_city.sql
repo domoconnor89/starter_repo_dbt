3d233a8a8d6e656b7008dd2ba4bfc9209131da0f
domoconnor89 <dominicoconnor89@googlemail.com>
2023-10-16 10:33:34 +0200

    mart update

--- models/mart/precip_by_city.sql
+++ models/mart/precip_by_city.sql
@@ -6,7 +6,7 @@ WITH avg_precip as (
         region,
         country,
         MAX(totalprecip_mm) as wk_day_maxprecip,
-        COUNT(totalprecip_mm WHERE totalprecip_mm > 0) as wkly_precip_occurrence,
+        --COUNT(totalprecip_mm WHERE totalprecip_mm > 0) as wkly_precip_occurrence,
         SUM(totalprecip_mm) as wkly_totalprecip
     FROM {{ref('prep_temp')}}
     GROUP BY year, week, city, region, country
