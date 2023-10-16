4384b9ef26944f9d924855d09668f325282c48ec
domoconnor89 <dominicoconnor89@googlemail.com>
2023-10-16 09:23:49 +0200

    prep update

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -2,6 +2,55 @@ WITH temp_daily AS (
     SELECT * 
     FROM {{ref('staging_weather')}}
 ),
+time_cnvrt AS (
+    SELECT *,
+        TO_CHAR(date,'Day') AS weekday,
+        TO_CHAR(date,'DD') AS day_num,
+		TO_CHAR(date,'week') AS week,
+		TO_CHAR(date,'month') AS month,
+        TO_CHAR(date,'year') AS year,
+        REPLACE (moonrise, 'No moonrise', '00:01 AM') as moonrise,
+        REPLACE (moonset, 'No moonset', '00:01 AM') as moonset,
+        CAST(moon_illumination AS INTEGER) AS moon_illumination_num
+    FROM temp_daily
+),
+temp_update AS (
+    SELECT 
+        date,
+        weekday,
+        day_num,
+        week,
+        month,
+        year,
+        city,
+        region,
+        country,
+        lat,
+        lon,
+        TO_CHAR(TO_TIMESTAMP(sunrise, 'HH12:MI PM'), 'HH24:MI') AS sunrise_24hr,
+        TO_CHAR(TO_TIMESTAMP(sunset, 'HH12:MI PM'), 'HH24:MI') AS sunset_24hr,
+        TO_CHAR(TO_TIMESTAMP(moonrise, 'HH12:MI PM'), 'HH24:MI') AS moonrise_24hr,
+        TO_CHAR(TO_TIMESTAMP(moonset, 'HH12:MI PM'), 'HH24:MI') AS moonset_24hr,
+        moon_phase,
+        moon_illumination_num,
+        maxtemp_c,
+        avgtemp_c,
+        mintemp_c,
+        maxwind_kph,
+        totalprecip_mm,
+        avgvis_km,
+        avghumidity,
+        condition
+    FROM time_cnvrt
+)
+SELECT *
+FROM temp_update
+
+/*
+WITH temp_daily AS (
+    SELECT * 
+    FROM {{ref('staging_weather')}}
+),
 UPDATE temp_daily
     SET moonrise = ''
         WHERE moonrise = 'No moonrise'
@@ -47,10 +96,13 @@ temp_update AS (
         country,
         lat,
         lon,
+        sunrise,
         --sunrise_24hr,
+        sunset,
         --sunset_24hr,
         moonrise,
         --moonrise_24hr,
+        moonset,
         --moonset_24hr,
         moon_phase,
         moon_illumination_num,
@@ -66,3 +118,4 @@ temp_update AS (
 )
 SELECT *
 FROM temp_update
+*/
