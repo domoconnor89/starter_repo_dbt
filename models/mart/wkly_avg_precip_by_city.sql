cd495ae6055325a1cdfe78f845913bfaa75caff1
domoconnor89 <dominicoconnor89@googlemail.com>
2023-10-17 12:03:20 +0200

    precip mart update

--- models/mart/wkly_avg_precip_by_city.sql
+++ models/mart/wkly_avg_precip_by_city.sql
@@ -9,8 +9,8 @@ WITH avg_precip as (
         SUM(rainy_day) as wkly_precip_days,
         SUM(totalprecip_mm) as wkly_totalprecip,
         CASE
-            WHEN wkly_precip_days > 0 THEN 1/wkly_precip_days
-            WHEN wkly_precip_days = 0 THEN 0
+            WHEN SUM(rainy_day) > 0 THEN 1/SUM(rainy_day)
+            WHEN SUM(rainy_day) = 0 THEN 0
             END AS rainfall_spread
     FROM {{ref('prep_temp')}}
     GROUP BY year, week, city, region, country
